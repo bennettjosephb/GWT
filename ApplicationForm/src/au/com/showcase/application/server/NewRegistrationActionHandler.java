e2d6f2a4542d7d6b217f169fe7403a7cbb4f128d
bennettjosephb <bennettjosephb@gmail.com>
2014-01-28 21:44:15 +1100

    added captcha from session.
    getting captcha details from session for validation.

--- ApplicationForm/src/au/com/showcase/application/server/NewRegistrationActionHandler.java
+++ ApplicationForm/src/au/com/showcase/application/server/NewRegistrationActionHandler.java
@@ -1,22 +1,51 @@
 package au.com.showcase.application.server;
 
+import static nl.captcha.Captcha.NAME;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+import nl.captcha.Captcha;
+
 import com.gwtplatform.dispatch.server.actionhandler.ActionHandler;
+
 import au.com.showcase.application.client.account.NewRegistration;
 import au.com.showcase.application.client.account.NewRegistrationResult;
+
 import com.google.inject.Inject;
+import com.google.inject.Provider;
 import com.gwtplatform.dispatch.server.ExecutionContext;
 import com.gwtplatform.dispatch.shared.ActionException;
 
 public class NewRegistrationActionHandler implements
 		ActionHandler<NewRegistration, NewRegistrationResult> {
 
+	private Provider<HttpServletRequest> requestProvider;
+
+	private ServletContext servletContext;
+
+	private HttpServletRequest httpServletRequest;
+
+	private HttpSession httpSession = null;
+
 	@Inject
-	public NewRegistrationActionHandler() {
+	public NewRegistrationActionHandler(
+			Provider<HttpServletRequest> requestProvider,
+			ServletContext servletContext, HttpServletRequest httpServletRequest) {
+		super();
+		this.requestProvider = requestProvider;
+		this.servletContext = servletContext;
+		this.httpServletRequest = httpServletRequest;
+		httpSession = httpServletRequest.getSession();
 	}
 
 	@Override
 	public NewRegistrationResult execute(NewRegistration action,
 			ExecutionContext context) throws ActionException {
+
+		Captcha captcha = (Captcha) httpSession.getAttribute(NAME);
+
 		return null;
 	}
 
